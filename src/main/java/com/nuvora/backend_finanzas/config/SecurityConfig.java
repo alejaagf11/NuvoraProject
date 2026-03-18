0f261809ffc443cf36cd0e89463c23ee6880c32a
alejaagf11 <sofiaalejandragonzalez31@gmail.com>
2026-03-17 21:53:29 -0500

    Fix_b_SecurityConfigCORS

--- src/main/java/com/nuvora/backend_finanzas/config/SecurityConfig.java
+++ src/main/java/com/nuvora/backend_finanzas/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
@@ -18,6 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable()) // Desactiva CSRF para APIs
                 .authorizeHttpRequests(auth -> auth
                         // Endpoints públicos
@@ -30,5 +33,19 @@ public class SecurityConfig {
                 .formLogin(form -> form.disable()); // Desactiva login por formulario
 
         return http.build();
+    }
+
+    // config CORS Angular
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
