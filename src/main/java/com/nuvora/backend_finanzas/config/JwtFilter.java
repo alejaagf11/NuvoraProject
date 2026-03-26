4e47188fdeb5e2fc50acab24b605235b79b5832e
alejaagf11 <sofiaalejandragonzalez31@gmail.com>
2026-03-25 19:37:02 -0500

    Feature_b_JWT

---
+++ src/main/java/com/nuvora/backend_finanzas/config/JwtFilter.java
@@ -0,0 +1,42 @@
+package com.nuvora.backend_finanzas.config;
+
+import com.nuvora.backend_finanzas.security.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                  HttpServletResponse response,
+                                  FilterChain filterChain) throws ServletException, IOException{
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer")){
+            String token = header.substring(7); // se quita "Bearer"
+            try{
+                Long userId = jwtService.validateTokenAndGetUserId(token);
+                request.setAttribute("userId", userId);
+            }catch (Exception e){
+             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+             return;
+            }
+        }else{
+
+        }
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/nuvora/backend_finanzas/security/JwtService.java
@@ -0,0 +1,43 @@
+package com.nuvora.backend_finanzas.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.unknow}")
+    private String unknowKey;
+
+    @Value("${jwt.exp}")
+    private Long expTime;
+
+    private Key getSigningKey(){
+        return Keys.hmacShaKeyFor(unknowKey.getBytes());
+    }
+
+    public String createToken(Long usuarioId){
+        return Jwts.builder()
+                .setSubject(String.valueOf(usuarioId))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expTime))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public Long validateTokenAndGetUserId(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return Long.parseLong(claims.getSubject());
+
+    }
+
+}
