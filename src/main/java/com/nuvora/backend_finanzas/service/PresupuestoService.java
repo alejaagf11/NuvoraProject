7ec77cd8e21434f5e034465cb194b9227310863c
alejaagf11 <sofiaalejandragonzalez31@gmail.com>
2026-04-05 17:28:07 -0500

    FIX_B_SERVICE

--- src/main/java/com/nuvora/backend_finanzas/service/PresupuestoService.java
+++ src/main/java/com/nuvora/backend_finanzas/service/PresupuestoService.java
@@ -2,7 +2,9 @@ package com.nuvora.backend_finanzas.service;
 
 import com.nuvora.backend_finanzas.dto.BudgetDTO;
 import com.nuvora.backend_finanzas.entity.Usuario;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface PresupuestoService {
     BudgetDTO.Response generarPresupuesto(BudgetDTO.Request request, Usuario usuario);
 }
