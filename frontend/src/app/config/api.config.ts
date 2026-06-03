efe64bbfbe11523405678fec882a067f01a8c86b
sariiss <sagomezpineros30@gmail.com>
2026-06-03 18:15:00 -0500

    cambio localhost

--- frontend/src/app/config/api.config.ts
+++ frontend/src/app/config/api.config.ts
@@ -1,6 +1,5 @@
 import { Capacitor } from '@capacitor/core';
-
-const WEB_API_BASE_URL = 'http://localhost:8080';
+const WEB_API_BASE_URL = 'https://nuvoraproject-production.up.railway.app';
 const ANDROID_EMULATOR_API_BASE_URL = 'http://10.0.2.2:8080';
 
 export const API_BASE_URL = Capacitor.isNativePlatform()
